34fb21318574983992c1c603c33aa4377b40b890
yk <oncwnuMpIW7ssBqxS3fg9TldFlvY@git.weixin.qq.com>
2019-01-23 17:55:45 +0800

    first commit

---
+++ src/pages/counter/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="counter-warp">
+    <p>Vuex counter：{{ count + 2}}</p>
+    <p>
+      <i-button type="primary" @click="increment">+</i-button>
+      <i-button type="error" @click="decrement">-</i-button>
+    </p>
+
+    <a href="/pages/index/main" class="home">去往首页</a>
+  </div>
+</template>
+
+<script>
+// Use Vuex
+import store from './store'
+
+export default {
+  computed: {
+    count () {
+      return store.state.count
+    }
+  },
+  methods: {
+    increment () {
+      store.commit('increment')
+    },
+    decrement () {
+      store.commit('decrement')
+    }
+  }
+}
+</script>
+
+<style>
+.counter-warp {
+  text-align: center;
+  margin-top: 100px;
+}
+.home {
+  display: inline-block;
+  margin: 100px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
+}
+</style>
